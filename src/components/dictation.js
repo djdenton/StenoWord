35a293904d3ab54dc282bf87bfcd26e889b0a298
Dave Dean <deano1@gmail.com>
2018-10-08 22:30:23 +0100

    minor edits to css

--- src/components/dictation.js
+++ src/components/dictation.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 function DICTATION(props){
     const dictation = props.dictation;
-    const listSections = Object.keys(dictation).map((key) => <li key={key}>{dictation[key]}</li>);
+    const listSections = Object.keys(dictation).map((key) => <div key={key}>{dictation[key]}</div>);
   
     return(
       <div className="innerbox3">
